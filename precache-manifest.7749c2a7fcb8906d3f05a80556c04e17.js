e527bbbe1594e13b246a55bf6c0f463331847388
Ryan Gonzales <helloryantg@gmail.com>
2018-12-05 13:59:19 -0800

    Updates

---
+++ precache-manifest.7749c2a7fcb8906d3f05a80556c04e17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee902e61a5a89ae0d65b",
+    "url": "/project-4-assessment/static/js/runtime~main.ee902e61.js"
+  },
+  {
+    "revision": "de5fa76f6858a1194762",
+    "url": "/project-4-assessment/static/js/main.de5fa76f.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/project-4-assessment/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "de5fa76f6858a1194762",
+    "url": "/project-4-assessment/static/css/main.bbf64829.chunk.css"
+  },
+  {
+    "revision": "910bee6f5c42e71709ad4cdb72ac2923",
+    "url": "/project-4-assessment/index.html"
+  }
+];
